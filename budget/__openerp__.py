9dfe4efddb87aaf58155b1b57a10362b39b7aaec
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2014-08-05 17:52:38 +0200

    bump version

--- budget/__openerp__.py
+++ budget/__openerp__.py
@@ -19,7 +19,7 @@
 #
 ##############################################################################
 {"name": "Multicurrency Analytic Budget",
- "version": "1.0",
+ "version": "1.1",
  "author": "Camptocamp",
  "category": "Generic Modules/Accounting",
  "website": "http://camptocamp.com",
