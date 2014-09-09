927b31669a940d681b63c476cc32110d3549f2ba
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2014-09-09 17:29:12 +0200

    document the active version logic

--- budget/__openerp__.py
+++ budget/__openerp__.py
@@ -34,6 +34,16 @@ Features:
 
 This module is for real advanced budget use, otherwise prefer to use the
 OpenERP official one.
+
+Active Budget Version
+=====================
+
+Every Budget has zero or one active Version. This is marked by a flag and that
+can be set with a button. All other versions are disabled.
+
+If a budget version is duplicated, the old one is automatically disabled as
+well.
+
     """,
  "complexity": "expert",
  "depends": ["base",
