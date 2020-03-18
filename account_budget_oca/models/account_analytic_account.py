7eafbcbc57c09b98c2fb7f827ca079abea64a6de
amcor <alberto.martin@guadaltech.es>
2020-03-18 15:49:02 +0100

    [IMP] account_budget_oca: black, isort

--- account_budget_oca/models/account_analytic_account.py
+++ account_budget_oca/models/account_analytic_account.py
@@ -7,5 +7,7 @@ class AccountAnalyticAccount(models.Model):
     _inherit = "account.analytic.account"
 
     crossovered_budget_line_ids = fields.One2many(
-        comodel_name='crossovered.budget.lines',
-        inverse_name='analytic_account_id', string='Budget Lines')
+        comodel_name="crossovered.budget.lines",
+        inverse_name="analytic_account_id",
+        string="Budget Lines",
+    )
